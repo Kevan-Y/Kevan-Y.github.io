653f92dee757317dee61ed22025b40e1e4ede20f
Kevan <kevanew@hotmail.com>
2020-04-14 14:48:48 -0400

    Updated

--- AcesStore/script/item.js
+++ AcesStore/script/item.js
@@ -2,154 +2,154 @@ window.items = [{
     name: 'T-shirt JP A',
     category: 'JP',
     price: 15.99,
-    picURL: '/images/TShirt_JP_A.png',
+    picURL: '/AcesStore/images/TShirt_JP_A.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt JP I',
     category: 'JP',
     price: 15.99,
-    picURL: '/images/TShirt_JP_I.png',
+    picURL: '/AcesStore/images/TShirt_JP_I.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt JP U',
     category: 'JP',
     price: 15.99,
-    picURL: '/images/TShirt_JP_U.png',
+    picURL: '/AcesStore/images/TShirt_JP_U.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt JP E',
     category: 'JP',
     price: 15.99,
-    picURL: '/images/TShirt_JP_E.png',
+    picURL: '/AcesStore/images/TShirt_JP_E.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt JP O',
     category: 'JP',
     price: 15.99,
-    picURL: '/images/TShirt_JP_O.png',
+    picURL: '/AcesStore/images/TShirt_JP_O.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN A',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_A.png',
+    picURL: '/AcesStore/images/TShirt_EN_A.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN B',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_B.png',
+    picURL: '/AcesStore/images/TShirt_EN_B.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN C',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_C.png',
+    picURL: '/AcesStore/images/TShirt_EN_C.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN D',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_D.png',
+    picURL: '/AcesStore/images/TShirt_EN_D.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN E',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_E.png',
+    picURL: '/AcesStore/images/TShirt_EN_E.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN F',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_F.png',
+    picURL: '/AcesStore/images/TShirt_EN_F.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN G',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_G.png',
+    picURL: '/AcesStore/images/TShirt_EN_G.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN H',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_H.png',
+    picURL: '/AcesStore/images/TShirt_EN_H.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN I',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_I.png',
+    picURL: '/AcesStore/images/TShirt_EN_I.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt EN J',
     category: 'EN',
     price: 10.99,
-    picURL: '/images/TShirt_EN_J.png',
+    picURL: '/AcesStore/images/TShirt_EN_J.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt Music 1',
     category: 'MUSIC',
     price: 21.99,
-    picURL: '/images/TShirt_MUSIC_1.png',
+    picURL: '/AcesStore/images/TShirt_MUSIC_1.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt Music 2',
     category: 'MUSIC',
     price: 21.99,
-    picURL: '/images/TShirt_MUSIC_2.png',
+    picURL: '/AcesStore/images/TShirt_MUSIC_2.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt Music 3',
     category: 'MUSIC',
     price: 21.99,
-    picURL: '/images/TShirt_MUSIC_3.png',
+    picURL: '/AcesStore/images/TShirt_MUSIC_3.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt Music 4',
     category: 'MUSIC',
     price: 21.99,
-    picURL: '/images/TShirt_MUSIC_4.png',
+    picURL: '/AcesStore/images/TShirt_MUSIC_4.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt Music 5',
     category: 'MUSIC',
     price: 21.99,
-    picURL: '/images/TShirt_MUSIC_5.png',
+    picURL: '/AcesStore/images/TShirt_MUSIC_5.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt Music 6',
     category: 'MUSIC',
     price: 21.99,
-    picURL: '/images/TShirt_MUSIC_6.png',
+    picURL: '/AcesStore/images/TShirt_MUSIC_6.png',
     description: 'Description.......'
   },
   {
     name: 'T-shirt Music 7',
     category: 'MUSIC',
     price: 21.99,
-    picURL: '/images/TShirt_MUSIC_7.png',
+    picURL: '/AcesStore/images/TShirt_MUSIC_7.png',
     description: 'Description.......'
   }
 ];
